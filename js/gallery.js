30d9d0e527e9802abeaabfdd1b40abff885a06bf
Eric Powell <ericpowell03@gmail.com>
2020-08-26 15:54:17 -0600

    preload images

--- js/gallery.js
+++ js/gallery.js
@@ -5,8 +5,14 @@ function activateGallery() {
   let captionTitle = caption.querySelector(".title");
   let captionDescription = caption.querySelector(".description");
   // either notation style above works for finding child selectors. Carrots find direct descendents.
-  
+
   thumbnails.forEach(function(thumbnail) {
+    // Preload the large images;
+    let largeVersion = new Image();
+    let newImageSrc = thumbnail.dataset.largeVersion;
+    largeVersion.src = newImageSrc;
+      
+
     thumbnail.addEventListener("click", function () {
       // set clickced image as main image.
       mainImage.setAttribute("src", thumbnail.dataset.largeVersion);
