5cfc7e57fddbbe8c01c26b562f881410aef7d730
Eric Powell <ericpowell03@gmail.com>
2020-08-26 15:50:15 -0600

    update caption and main image and alt

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,21 @@
 function activateGallery() {
-  alert("hello");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage = document.querySelector("#gallery-photo img");
+  let caption = document.querySelector("#gallery-info");
+  let captionTitle = caption.querySelector(".title");
+  let captionDescription = caption.querySelector(".description");
+  // either notation style above works for finding child selectors. Carrots find direct descendents.
+
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function () {
+      // set clickced image as main image.
+      mainImage.setAttribute("src", thumbnail.dataset.largeVersion);
+      captionTitle.innerHTML = thumbnail.dataset.title;
+      captionDescription.innerHTML = thumbnail.dataset.description;
+      mainImage.setAttribute("alt", thumbnail.getAttribute("alt"));
+      let currentThumbnail = document.querySelector(".current");
+      currentThumbnail.classList.remove("current");
+      thumbnail.parentNode.classList.add("current");
+    });
+  });
 }
