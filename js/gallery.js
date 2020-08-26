020fd1e493744a62df92744aa85bd851b468f5ce
Eric Powell <ericpowell03@gmail.com>
2020-08-26 16:02:24 -0600

    refactor and clean up a bit

--- js/gallery.js
+++ js/gallery.js
@@ -1,9 +1,9 @@
 function activateGallery() {
-  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
-  let mainImage = document.querySelector("#gallery-photo img");
-  let caption = document.querySelector("#gallery-info");
-  let captionTitle = caption.querySelector(".title");
-  let captionDescription = caption.querySelector(".description");
+  let thumbnails          = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage           = document.querySelector("#gallery-photo img");
+  let caption             = document.querySelector("#gallery-info");
+  let captionTitle        = caption.querySelector(".title");
+  let captionDescription  = caption.querySelector(".description");
   // either notation style above works for finding child selectors. Carrots find direct descendents.
 
   thumbnails.forEach(function(thumbnail) {
@@ -11,16 +11,18 @@ function activateGallery() {
     let largeVersion = new Image();
     let newImageSrc = thumbnail.dataset.largeVersion;
     largeVersion.src = newImageSrc;
-      
-
+    
     thumbnail.addEventListener("click", function () {
-      // set clickced image as main image.
+      // set clickced image as main image and update image alt.
       mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", thumbnail.getAttribute("alt"));
+      
+      // set the caption
       captionTitle.innerHTML = thumbnail.dataset.title;
       captionDescription.innerHTML = thumbnail.dataset.description;
-      mainImage.setAttribute("alt", thumbnail.getAttribute("alt"));
-      let currentThumbnail = document.querySelector(".current");
-      currentThumbnail.classList.remove("current");
+      
+      // toggle current class on selected thumb.
+      document.querySelector(".current").classList.remove("current");
       thumbnail.parentNode.classList.add("current");
     });
   });
